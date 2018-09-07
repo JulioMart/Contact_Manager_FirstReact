7b1de9281b943c67283e24d1e6ffd9e6645384dd
JulioMart <juliomartelosso@gmail.com>
2018-09-07 20:31:42 +0200

    errors

--- src/components/contact/AddContact.js
+++ src/components/contact/AddContact.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import { Consumer } from '../../context';
-import uuid from 'uuid';
-
 import TextInputGroup from '../layout/TextInputGroup';
+
+import uuid from 'uuid';
 
 class AddContact extends Component {
 	state = {
 		name: '',
 		email: '',
-		phone: ''
+		phone: '',
+		errors: {}
 	};
 
 	onChange = (e) => {
@@ -19,6 +20,20 @@ class AddContact extends Component {
 		e.preventDefault();
 
 		const { name, email, phone } = this.state;
+
+		//Errors
+		if (name === '') {
+			this.setState({ errors: { name: 'Name is required' } });
+			return;
+		}
+		if (email === '') {
+			this.setState({ errors: { email: 'Email is required' } });
+			return;
+		}
+		if (phone === '') {
+			this.setState({ errors: { phone: 'Phone is required' } });
+			return;
+		}
 
 		const newContact = {
 			id: uuid(),
@@ -32,12 +47,13 @@ class AddContact extends Component {
 		this.setState({
 			name: '',
 			email: '',
-			phone: ''
+			phone: '',
+			errors: {}
 		});
 	};
 
 	render() {
-		const { name, email, phone } = this.state;
+		const { name, email, phone, errors } = this.state;
 
 		return (
 			<Consumer>
@@ -55,14 +71,16 @@ class AddContact extends Component {
 										placeholder="Enter name ..."
 										value={name}
 										onChange={this.onChange}
+										error={errors.name}
 									/>
 									<TextInputGroup
 										label="Email"
-                    name="email"
-                    type='email'
+										name="email"
+										type="email"
 										placeholder="Enter email ..."
 										value={email}
 										onChange={this.onChange}
+										error={errors.email}
 									/>
 									<TextInputGroup
 										label="Phone"
@@ -70,6 +88,7 @@ class AddContact extends Component {
 										placeholder="Enter phone number ..."
 										value={phone}
 										onChange={this.onChange}
+										error={errors.phone}
 									/>
 									{/* <div className="form-group">
 										<label htmlFor="email">Email</label>
@@ -82,7 +101,7 @@ class AddContact extends Component {
 											onChange={this.onChange}
 										/>
 									</div> */}
-									
+
 									<input type="submit" className="btn btn-light btn-block" value="Add Contact" />
 								</form>
 							</div>
